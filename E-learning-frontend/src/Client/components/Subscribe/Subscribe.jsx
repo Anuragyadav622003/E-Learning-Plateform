e6320ac31a660896f265baa64a88f1b4b1960d67
Anurag yadav <anuragyadav622003@gmail.com>
2024-09-30 00:44:25 +0530

    ch

---
+++ E-learning-frontend/src/Client/components/Subscribe/Subscribe.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { FaBell } from "react-icons/fa";
+import BgImage from "../../components/Subscribe/sub.png";
+
+import {motion} from "framer-motion";
+const bgStyle ={
+    backgroundImage:`url{$(BgImage)}`,
+    backgroundRepeat:"no-repeat",
+    backgroundSize:"cover",
+    backgroundPosition:"center"
+};
+
+const Subscribe = () => {
+  return (
+    <div className='bg-[#f4f4f4] '>
+    <div className='container '>
+    
+    
+    
+    </div>
+    
+    </div>
+  )
+}
+
+export default Subscribe
