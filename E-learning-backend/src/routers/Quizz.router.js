68d9392d0e873bdfa8d47b1f8a644645aeb254c7
Ryzen XP <iron4540@gmail.com>
2024-10-28 01:17:11 +0530

    Modify login and  add logout  function in user.controller

--- E-learning-backend/src/routers/Quizz.router.js
+++ E-learning-backend/src/routers/Quizz.router.js
@@ -1,5 +1,9 @@
 import express from 'express';
+<<<<<<< HEAD:E-learning-backend/src/routers/Quizzes.js
+import { createQuizze, getAllQuizzes } from '../controllers/QuizzesController.js';
+=======
 import { getAllQuizzes } from '../controllers/Quizz.controller.js';
+>>>>>>> 9b8698da94d2ec8049c54281f3971dc36516e691:E-learning-backend/src/routers/Quizz.router.js
 const router = express.Router();
 //router.post('/quiz', quizController.createQuiz);         // Create a new quiz
 router.get('/quizzes', getAllQuizzes);       // Get all quizzes
