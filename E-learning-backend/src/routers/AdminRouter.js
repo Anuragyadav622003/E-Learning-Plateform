b28b21c445e32be8522c992943857e200fe95d22
Anurag yadav <134534151+Anuragyadav622003@users.noreply.github.com>
2024-11-12 10:53:11 +0530

    Delete E-learning-backend directory

--- E-learning-backend/src/routers/AdminRouter.js
+++
@@ -1,7 +0,0 @@
-import express from 'express';
-import { getDashboardData, getUserStatistics } from '../controllers/DashboardController.js';
-
-const router = express.Router();
-router.get('/dashboard',getDashboardData);
-router.get('/user-stats',getUserStatistics);
-export default router;
