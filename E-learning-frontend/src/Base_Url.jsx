432f13e6625d82be5fa7e616831e661ea673ae09
Anurag yadav <anuragyadav622003@gmail.com>
2024-11-16 21:02:56 +0530

    add backend

--- E-learning-frontend/src/Base_Url.jsx
+++ E-learning-frontend/src/Base_Url.jsx
@@ -1,2 +1,4 @@
-const Base_Url ="http://localhost:5000"||"https://e-learning-plateform-backend.vercel.app";
+
+// const Base_Url = "http://localhost:5000";
+const Base_Url = "https://e-learning-plateform-468p.vercel.app"
 export default Base_Url;
