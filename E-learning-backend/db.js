87b353d152cb5a86e6f0d8a7cbd124f1e273e5ef
Anurag yadav <anuragyadav622003@gmail.com>
2025-01-06 17:53:36 +0530

    changes

--- E-learning-backend/db.js
+++ E-learning-backend/db.js
@@ -10,10 +10,12 @@ const connectDB = async () => {
     await mongoose.connect(mongoURI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
-    }) 
+    }) .then(() => console.log('Connected to MongoDB'))
+    .catch(err => console.error('Could not connect to MongoDB', err));;  
+    console.log('MongoDB connected established');
   } catch (err) {
     console.error('MongoDB connection error:', err);
-  
+   
   }
 };
 
